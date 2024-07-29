d28067b49d5b1be8134388da9bae37bbaa5d0a71
Guilherme Almeida <guimathiasgui@gmail.com>
2024-07-29 02:29:14 -0300

    Adicionando DTO

---
+++ GerenciadorTarefas.Application/DTOs/ComentarioDto.cs
@@ -0,0 +1,10 @@
+﻿namespace GerenciadorTarefas.Application.DTOs
+{
+    public class ComentarioDto
+    {
+        public int Id { get; set; }
+        public string? Conteudo { get; set; }
+        public DateTime Data { get; set; }
+        public required int TarefaId { get; set; }
+    }
+}
